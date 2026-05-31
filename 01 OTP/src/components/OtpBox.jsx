6ba2ee257cc1b4b0ed28d1937d213861b2846c54
Abhay kumar <abhayrobotics1908@gmail.com>
2026-06-01 01:01:46 +0530

    delete functionality peending

--- 01 OTP/src/components/OtpBox.jsx
+++ 01 OTP/src/components/OtpBox.jsx
@@ -1,57 +1,69 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const OtpBox = ({ length }) => {
-    const [secret, setSecret] = useState(new Array(5).fill(""))
-    const [focusId,setFocusId] = useState(0);
-    // manual otp check
-    const password=12345
+  const [secret, setSecret] = useState(new Array(5).fill(""));
+  const [focusId, setFocusId] = useState(0);
+  // manual otp check
+  const password = 12345;
 
-    // add the focus based on focus id, default is 0
-    useEffect(()=>{
-        if(focusId<length){
-
-            const element1 = document.getElementById("tab"+focusId)
-            element1.focus()
-        }
-        else{
-            password==secret.join("")?
-            console.log("Login successful"):
-            console.log("Login unsuccessful")
-        }
-    },[focusId])
-
-    const handleChange =(e,id)=>{
-        try{
-
-            let updatedArray = [...secret]
-            updatedArray[id]= e.currentTarget.value
-            console.log(secret,updatedArray,secret[id])
-            setSecret(updatedArray)
-            setFocusId((prev)=>prev+1)
-        }
-        catch(e){
-            console.log(e)
-        }
-
+  // add the focus based on focus id, default is 0
+  useEffect(() => {
+    if (focusId < length) {
+      const element1 = document.getElementById("tab" + focusId);
+      element1.focus();
+    } else {
+      password == secret.join("")
+        ? console.log("Login successful")
+        : console.log("Login unsuccessful");
     }
+  }, [focusId]);
 
-    // pending handle change
-    return (
-        <div>
-            <div className=" flex">
-                {/* {secret} */}
-                {
-                    secret.map((item, id) => {
-                        return (
+  // adding the otp no. in blank field
+  const handleChange = (e, id) => {
+    try {
+      let updatedArray = [...secret];
+      updatedArray[id] = e;
+      console.log(secret, updatedArray, secret[id]);
+      setSecret(updatedArray);
+      setFocusId((prev) => prev + 1);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-                            <input type="text" key={id}  id={"tab"+id}  className=" border border-amber-800 p-3 m-2 text-center rounded-lg w-10 h-10" maxLength={1} value={secret[id]} onChange={(e)=>handleChange(e,id)} />
+  //Handle delete the otp no.
+  const handleDelete = (id) => {
 
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
-}
+    // console.log("delete triggered",id,focusId)
+    // handleChange("",id)
+    // setFocusId((prev)=>prev-2)
+  };
+//   !pending
 
-export default OtpBox
+  // pending handle change
+  return (
+    <div>
+      <div className=" flex">
+        {/* {secret} */}
+        {secret.map((item, id) => {
+          return (
+            <input
+              type="text"
+              key={id}
+              id={"tab" + id}
+              onKeyDown={(e) => {
+                if (e.key == "Backspace") handleDelete(id);
+              }}
+              className=" border border-amber-800 p-3 m-2 text-center rounded-lg w-10 h-10"
+              maxLength={1}
+              value={secret[id]}
+              onChange={(e) => handleChange(e.currentTarget.value, id)}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default OtpBox;
--- 01 OTP/src/components/Otp_window.jsx
+++ 01 OTP/src/components/Otp_window.jsx
@@ -2,41 +2,51 @@ import { useState } from "react";
 import OtpBox from "./OtpBox";
 
 const Otp_window = () => {
-
-  const [mobile, setMobile] = useState("")
-  const [submit, setSubmit] = useState(false)
-  const [alertMessage,setAlertMessage] =useState("")
+  const [mobile, setMobile] = useState("");
+  const [submit, setSubmit] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
   const handleSubmit = () => {
     const isValidMobile = /^\d{10}$/.test(mobile);
 
-    isValidMobile ?  setSubmit(mobile): setAlertMessage("Invalid mobile number");
-    console.log("handle submit activated", mobile, submit, isValidMobile)
-  }
+    isValidMobile
+      ? setSubmit(mobile)
+      : setAlertMessage("Invalid mobile number");
+    console.log("handle submit activated", mobile, submit, isValidMobile);
+  };
 
   return (
-
     <div className="flex h-dvh justify-center border border-amber-600">
       <div className="flex  my-24 items-center  flex-col  w-1/2  min-w-96   border">
-
         <h1 className="text-amber-600 text-3xl  m-5 ">Login</h1>
 
-        {
-          submit ?
-            <OtpBox length={5} /> :
-            <div className="m-5 flex flex-col items-center">
-
-              <input type="text" placeholder="Mobile no." className="border rounded-lg p-2" maxLength={10} onChange={(e) => setMobile(e.currentTarget.value)} value={mobile} ></input>
-              <button type="button" onClick={handleSubmit} className="my-3 w-28 text-center text-lg rounded-3xl hover:bg-amber-900   duration-100 border border-amber-800 cursor-pointer p-2">Submit</button>
-              <div className="text-red-700" >{alertMessage}</div>
-            </div>
-        }
-        
-
-
+        {submit ? (
+          <OtpBox length={5} />
+        ) : (
+          <div className="m-5 flex flex-col items-center">
+            <input
+              type="text"
+              placeholder="Mobile no."
+              className="border rounded-lg p-2"
+              onKeyDown={(e) => {
+                if (e.key === "Enter") handleSubmit();
+              }}
+              maxLength={10}
+              onChange={(e) => setMobile(e.currentTarget.value)}
+              value={mobile}
+            ></input>
+            <button
+              type="button"
+              onClick={handleSubmit}
+              className="my-3 w-28 text-center text-lg rounded-3xl hover:bg-amber-900   duration-100 border border-amber-800 cursor-pointer p-2"
+            >
+              Submit
+            </button>
+            <div className="text-red-700">{alertMessage}</div>
+          </div>
+        )}
       </div>
     </div>
-
-  )
+  );
 };
 
 export default Otp_window;
