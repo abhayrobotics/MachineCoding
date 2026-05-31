5b610320f27f389c700104796ae51b90eafe9057
Abhay kumar <abhayrobotics1908@gmail.com>
2026-06-01 00:34:50 +0530

    focus changing

--- 01 OTP/src/components/OtpBox.jsx
+++ 01 OTP/src/components/OtpBox.jsx
@@ -1,23 +1,50 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const OtpBox = ({ length }) => {
     const [secret, setSecret] = useState(new Array(5).fill(""))
+    const [focusId,setFocusId] = useState(0);
+    // manual otp check
+    const password=12345
 
-    // const handleChange =()=>{
-    //     let updatedArray = secret
+    // add the focus based on focus id, default is 0
+    useEffect(()=>{
+        if(focusId<length){
 
-    // }
+            const element1 = document.getElementById("tab"+focusId)
+            element1.focus()
+        }
+        else{
+            password==secret.join("")?
+            console.log("Login successful"):
+            console.log("Login unsuccessful")
+        }
+    },[focusId])
+
+    const handleChange =(e,id)=>{
+        try{
+
+            let updatedArray = [...secret]
+            updatedArray[id]= e.currentTarget.value
+            console.log(secret,updatedArray,secret[id])
+            setSecret(updatedArray)
+            setFocusId((prev)=>prev+1)
+        }
+        catch(e){
+            console.log(e)
+        }
+
+    }
 
     // pending handle change
     return (
         <div>
             <div className=" flex">
-                {secret}
+                {/* {secret} */}
                 {
                     secret.map((item, id) => {
                         return (
 
-                            <input type="text" key={id} className=" border border-amber-800 p-3 m-2 rounded-lg w-10 h-10" maxLength={1} value={secret[id]} onChange={(e)=> setSecret()} />
+                            <input type="text" key={id}  id={"tab"+id}  className=" border border-amber-800 p-3 m-2 text-center rounded-lg w-10 h-10" maxLength={1} value={secret[id]} onChange={(e)=>handleChange(e,id)} />
 
                         )
                     })
--- 01 OTP/src/components/Otp_window.jsx
+++ 01 OTP/src/components/Otp_window.jsx
@@ -5,11 +5,11 @@ const Otp_window = () => {
 
   const [mobile, setMobile] = useState("")
   const [submit, setSubmit] = useState(false)
-
+  const [alertMessage,setAlertMessage] =useState("")
   const handleSubmit = () => {
     const isValidMobile = /^\d{10}$/.test(mobile);
 
-    isValidMobile ?  setSubmit(mobile): alert("Wrong Mobile no.");
+    isValidMobile ?  setSubmit(mobile): setAlertMessage("Invalid mobile number");
     console.log("handle submit activated", mobile, submit, isValidMobile)
   }
 
@@ -27,8 +27,10 @@ const Otp_window = () => {
 
               <input type="text" placeholder="Mobile no." className="border rounded-lg p-2" maxLength={10} onChange={(e) => setMobile(e.currentTarget.value)} value={mobile} ></input>
               <button type="button" onClick={handleSubmit} className="my-3 w-28 text-center text-lg rounded-3xl hover:bg-amber-900   duration-100 border border-amber-800 cursor-pointer p-2">Submit</button>
+              <div className="text-red-700" >{alertMessage}</div>
             </div>
         }
+        
 
 
       </div>
