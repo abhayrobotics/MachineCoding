08a6f04d1ae289124f71ea7b065dcf7c3298cf9c
PAKHI PRIYA <abhayrobotics1908@gmail.com>
2026-06-29 23:28:27 +0530

    servers js

--- 03_NODE/server.js
+++ 03_NODE/server.js
@@ -3,5 +3,20 @@ const  express = require("express")
 const app  = express()
 
 app.get("/", (req,res)=>{
-     app.send
+     res.send("hello")
+})
+
+app.get("/task",(req,res)=>{
+   res.send("task route")
+
+})
+
+app.get("/task/:taskId/order/:orderID",(req,res)=>{
+   res.send(`dynamic route; order=${req.params.orderID}
+      task=${req.params.taskId} params =${JSON.stringify(req.params)} url= ${req.url}`)
+      res.send('running')
+})
+
+app.listen(3000,()=>{
+   console.log("server is running")
 })
